52bab0f5393cde876de8c83542f472152f1a8f1a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-05-01 20:20:42 +0200

    add: @ngInject typescript

--- app/21_http/main.ts
+++ app/21_http/main.ts
@@ -5,13 +5,31 @@ declare const angular: angular.IAngularStatic;
 
 const app = angular.module('myApp', []);
 
-app.controller('MyController', function MyController($scope, $http) {
-	'ngInject';
-	$http.get('content.json').then((response) => {
-		$scope.content = response.data.content;
-		console.log('done');
-	}).catch((error) => {
-		$scope.content = '';
-		console.log('Error', error);
-	});
-});
+class MyTypeScriptCtrl {
+	/* @ngInject */
+	constructor(private $scope, private $http) {
+		this.doSomething();
+	}
+	public doSomething() {
+		this.$http.get('content.json').then((response) => {
+			this.$scope.content = response.data.content;
+			console.log('done');
+		}).catch((error) => {
+			this.$scope.content = '';
+			console.log('Error', error);
+		});
+	}
+}
+
+app.controller('MyController', MyTypeScriptCtrl);
+
+// app.controller('MyController', function MyController($scope, $http) {
+// 	'ngInject';
+// 	$http.get('content.json').then((response) => {
+// 		$scope.content = response.data.content;
+// 		console.log('done');
+// 	}).catch((error) => {
+// 		$scope.content = '';
+// 		console.log('Error', error);
+// 	});
+// });
