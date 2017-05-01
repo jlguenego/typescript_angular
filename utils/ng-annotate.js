c98f135bd48a78cffc02a396af6fa3bd4a97f40d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-05-01 20:00:05 +0200

    add: remove comments

--- utils/ng-annotate.js
+++ utils/ng-annotate.js
@@ -1,16 +1,16 @@
 var ts = require("ts");
-console.log('ts', ts);
+// console.log('ts', ts);
 var ngAnnotate = require("ng-annotate");
-console.log('ngAnnotate', ngAnnotate);
+// console.log('ngAnnotate', ngAnnotate);
 
 exports.translate = function translate(load) {
 	SystemJS.builder = true;
 	return Promise.resolve().then(function() {
 		return ts.translate(load);
 	}).then(function(response) {
-		console.log('response', response);
+		// console.log('response', response);
 		let r2 = ngAnnotate(response, {add: true});
-		console.log('r2.src', r2.src);
+		// console.log('r2.src', r2.src);
 		return r2.src;
 	});
 };
